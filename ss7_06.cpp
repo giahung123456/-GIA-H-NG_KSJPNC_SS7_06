4d5cb4cbd31c5b7b75099bb95a1ee289bbb0a4d0
giahung123456 <dogiahungtplc@gmail.com>
2024-11-29 03:48:51 +0700

    UPLOAD SS7_06

---
+++ ss7_06.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5}; 
+    printf("Mang ban dau: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] % 2 == 0) {
+            arr[i] += 3; 
+        } else {
+            arr[i] += 2; 
+        }
+    }
+
+        printf("\nMang sau khi thay doi: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+   
+}
